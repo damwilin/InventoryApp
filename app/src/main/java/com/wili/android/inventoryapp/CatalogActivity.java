2c81595646ccfcf69041f9305b96fd4d8ecf0d56
damwilin <damian.wilinski@gmail.com>
2017-09-21 22:34:03 +0200

    Added options menu in CatalogActivity

--- app/src/main/java/com/wili/android/inventoryapp/CatalogActivity.java
+++ app/src/main/java/com/wili/android/inventoryapp/CatalogActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -32,5 +34,25 @@ public class CatalogActivity extends AppCompatActivity {
         TextView emptyView = (TextView) findViewById(R.id.empty_view);
         //Set that view as empty view in ListView
         productsListView.setEmptyView(emptyView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.insert_dummy_data:
+                // TODO: 9/21/2017 InsertDummyData()
+                return true;
+            case R.id.delete_all_data:
+                // TODO: 9/21/2017 deleteAllData()
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
